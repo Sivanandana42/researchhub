4e61d47218347b0dad84726fd446f33f163c2a6c
Sivanandana42 <sivanandana366@gmail.com>
2026-06-09 10:56:14 +0530

    Create admin user on startup

--- app/apps.py
+++ app/apps.py
@@ -3,3 +3,17 @@ from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     name = 'app'
+
+    def ready(self):
+        try:
+            from django.contrib.auth.models import User
+
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser(
+                    username='admin',
+                    email='admin@example.com',
+                    password='Admin@123'
+                )
+                print("Admin user created")
+        except Exception:
+            pass
